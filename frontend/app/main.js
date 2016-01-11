b19c372968ff1ae83800d35b94b2fcb835ed3829
Luke Roberts <email@luke-roberts.co.uk>
2016-01-11 12:03:15 -0800

    ignore frontend build

--- frontend/app/main.js
+++ frontend/app/main.js
@@ -1,9 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-// Here we put our React instance to the global scope. Make sure you do not put it 
-// into production and make sure that you close and open your console if the 
-// DEV-TOOLS does not display
+var Redux = require('redux');
+
 window.React = React; 
 
+var store = Redux.createStore();
 var App = require('./components/App.jsx');
-ReactDOM.render(<App/>, document.getElementById('app'));
+
+var render = function(){
+  ReactDOM.render(<App/>, document.getElementById('app'));
+}
+
+// store.subscribe('render');
+render();
