6efdcf97c4ba710c10b547e5cad5c0a276112783
Tom Moor <tom.moor@gmail.com>
2016-01-23 13:19:17 -0800

    Vote buttons

--- static/app/components/RepoItem.jsx
+++ static/app/components/RepoItem.jsx
@@ -3,16 +3,18 @@ var RepoItem = React.createClass({
   render: function(){
     return (<li className="repo">
             <div className="details">
-              <h3>
+              <h3 className="name">
                 {this.props.repo.name}
                 <span className="stargazers" title={this.props.repo.stargazers_count + " Stargazers"}>
                   <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
                   {this.props.repo.stargazers_count} 
                 </span>
               </h3>
-              <p>{this.props.repo.description}</p>
+              <p className="description">{this.props.repo.description || "This submission has no description."}</p>
             </div>
-            <button className="btn btn-green" onClick={this.props.vote}>Vote</button>
+            <button className="btn btn-green" onClick={this.props.vote}>
+              Vote <span className="glyphicon glyphicon-arrow-up" aria-hidden="true"></span>
+            </button>
             </li>)
   }
 
