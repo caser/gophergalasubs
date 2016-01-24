c3fd48c06786d26603980eac773230e21b06d3ee
Luke Roberts <email@luke-roberts.co.uk>
2016-01-24 12:27:22 -0800

    make titles link to repso

--- static/app/components/RepoItem.jsx
+++ static/app/components/RepoItem.jsx
@@ -42,7 +42,7 @@ var RepoItem = React.createClass({
     return (<li className="repo">
             <div className="details">
               <h3 className="name">
-                {this.props.repo.name}
+                <a href={this.props.repo.html_url} target="_blank">{this.props.repo.name}</a>
                 <span className="stargazers" title={this.props.repo.stargazers_count + " Stargazers"}>
                   <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
                   {this.props.repo.stargazers_count} 
