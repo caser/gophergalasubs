2c86ab911211e2b518dc77101ec518d3ee34a06d
Luke Roberts <email@luke-roberts.co.uk>
2016-01-24 12:11:36 -0800

    make login button more explicit

--- static/app/components/Login.jsx
+++ static/app/components/Login.jsx
@@ -3,7 +3,7 @@ var Login = React.createClass({
     return (<div id="login-portal">
             <img src="build/images/fancy-gopher.jpg" width="100px" />
             <h1>Login To Submit Your Gopher Gala 2016 Top 5</h1>
-            <a href="/login" className="btn btn-green">Login</a>
+            <a href="/login" className="btn btn-green">Login With Github</a>
             </div>
            );
   }
