485011dde0329db3b9d019b9854772c7bf72f6dc
Luke Roberts <email@luke-roberts.co.uk>
2016-01-24 12:09:46 -0800

    style login page

--- static/app/components/Login.jsx
+++ static/app/components/Login.jsx
@@ -1,8 +1,9 @@
 var Login = React.createClass({
   render: function() {
-    return (<div>
-            <h2>Login</h2>
-            <a href="/login">Login</a>
+    return (<div id="login-portal">
+            <img src="build/images/fancy-gopher.jpg" width="100px" />
+            <h1>Login To Submit Your Gopher Gala 2016 Top 5</h1>
+            <a href="/login" className="btn btn-green">Login</a>
             </div>
            );
   }
